f4d65b4f62d6ae7c557952c6bc65da0d03e31ae6
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-06-25 09:39:30 +0000

    nett

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 
 db_path = 'film-grab-db.sqlite3'
 # os.remove(db_path)
-os.rename(db_path, f'backup-{db_path}')
+# os.rename(db_path, f'backup-{db_path}')
 
 def init_db(db):
     stmts = [
@@ -23,62 +23,77 @@ def init_db(db):
         db.execute(s)
     
 
-with sqlite3.connect(db_path) as db:
-    init_db(db)
+# with sqlite3.connect(db_path) as db:
+#     init_db(db)
+
+# driver = webdriver.Safari(keep_alive=False)
+options = webdriver.ChromeOptions()
+options.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+options.add_argument('--headless')
+driver = webdriver.Chrome(executable_path='/Users/odinndagur/Downloads/chromedriver_mac64(1)/chromedriver',options=options)
+base_url = 'https://film-grab.com/movies-a-z/'
+
+# driver.get(base_url)
+# movie_elements = driver.find_elements_by_class_name('listing-item')
+# def get_movie(el):
+#     print(el.text)
+#     return (el.text, el.find_element_by_class_name('title').get_attribute('href'))
+with open('movies.txt','r') as f:
+    movies = [line.split(' - ') for line in f.readlines()]
+    print(f'{len(movies)} movies')
+    # print(movies[1289])
     # quit()
-    # print(db.execute('select * from sqlite_master').fetchall())
-    # quit()
+# print(movies[1076])
+# quit()
+# movies = [get_movie(el) for el in movie_elements]
+# with open('movies.txt','w') as f:
+#     f.writelines([f'{title} - {filmgrab_url}\n' for title, filmgrab_url in movies])
+for title, filmgrab_url in movies[1290:]:
+    # title = movie.text
+    # link = movie.find_element_by_class_name('title')
 
-    driver = webdriver.Safari(keep_alive=False)
-    # driver.implicitly_wait(20)
-    # driver.set_page_load_timeout(30)
-    base_url = 'https://film-grab.com/movies-a-z/'
+    # filmgrab_url = link.get_attribute('href')
+    # print(f'{title=} {filmgrab_url=}')
+    # link.click()
+    driver.get(filmgrab_url)
+    time.sleep(5)
 
-    driver.get(base_url)
-    movies = driver.find_elements_by_class_name('listing-item')
-    for movie in movies:
-        title = movie.text
-        link = movie.find_element_by_class_name('title')
-
-        filmgrab_url = link.get_attribute('href')
-        print(f'{title=} {filmgrab_url=}')
-        link.click()
-        time.sleep(5)
-
-        p_elements = driver.find_elements_by_tag_name('p')
-        for el in p_elements:
-            if 'Director: ' in el.text:
-                director = el.text.split('Director: ')[1]
-                print(el.text)
-            if 'Director of Photography: ' in el.text:
-                director_of_photography = el.text.split('Director of Photography: ')[1]
-                print(el.text)
-            if 'Production Design: ' in el.text:
-                production_designer = el.text.split('Production Design: ')[1]
-                print(el.text)
-            if 'Costume Design: ' in el.text:
-                costume_designer = el.text.split('Costume Design: ')[1]
-                print(el.text)
-            if 'Year: ' in el.text:
-                year = el.text.split('Year: ')[1]
-                print(el.text)
-        time.sleep(5)
-        imgs = driver.find_elements_by_class_name('bwg_lightbox')
-        img_urls = [img.get_attribute('href') for img in imgs]
-        # print(imgs)
+    p_elements = driver.find_elements_by_tag_name('p')
+    for el in p_elements:
+        if 'Director: ' in el.text:
+            director = el.text.split('Director: ')[1]
+            # print(el.text)
+        if 'Director of Photography: ' in el.text:
+            director_of_photography = el.text.split('Director of Photography: ')[1]
+            # print(el.text)
+        if 'Production Design: ' in el.text:
+            production_designer = el.text.split('Production Design: ')[1]
+            # print(el.text)
+        if 'Costume Design: ' in el.text:
+            costume_designer = el.text.split('Costume Design: ')[1]
+            # print(el.text)
+        if 'Year: ' in el.text:
+            year = el.text.split('Year: ')[1]
+            # print(el.text)
+    time.sleep(5)
+    imgs = driver.find_elements_by_class_name('bwg_lightbox')
+    img_urls = [img.get_attribute('href') for img in imgs]
+    # print(imgs)
 
 
-        print(f'''
-            {title=}
-            {filmgrab_url=}
-            {director=}
-            {director_of_photography=}
-            {production_designer=}
-            {costume_designer=}
-            {year=}
-            {img_urls=}
-            ''')
-        
+    print(f'''
+          
+        {title=}
+        {filmgrab_url=}
+        {director=}
+        {director_of_photography=}
+        {production_designer=}
+        {costume_designer=}
+        {year=}
+        {img_urls=}
+        ''')
+    with sqlite3.connect(db_path) as db:
+
         if not db.execute('SELECT * FROM director WHERE name = ?',[director]).fetchall():
             db.execute('INSERT INTO director(name) VALUES(?)',[director])
         if not db.execute('SELECT * FROM director_of_photography WHERE name = ?',[director_of_photography]).fetchall():
@@ -102,7 +117,6 @@ with sqlite3.connect(db_path) as db:
         ''',[title,year,filmgrab_url,director,director_of_photography,production_designer,costume_designer])
 
         db.executemany('INSERT INTO movie_image(movie_id,image_url) SELECT movie.id, ? from movie where movie.title = ?',[(img_url,title) for img_url in img_urls])
-        # 'CREATE TABLE movie_image(movie_id,image_url);'
 
-        driver.back()
-        
+    driver.back()
+    
